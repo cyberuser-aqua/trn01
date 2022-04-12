2d9e1fe719433abd2a4d3df71e775345a43a2a75
Mr. Aqua <cyberuser.aqua@gmail.com>
2022-04-12 17:31:53 +0300

    finalize

--- fmap.py
+++ fmap.py
@@ -1,18 +1,24 @@
 #!/bin/python3
 import argparse
 from os import path
+from typing import List
+
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument('pid', help='(PID) Process ID to print maps for', type=int)
+    parser.add_argument(
+        'pid', help='(PID) Process ID to print maps for', type=int)
     return parser.parse_args()
 
-def get_proc_maps(pid: int): 
+
+def get_proc_maps(pid: int) -> List[List[str]]:
     with open(f'/proc/{pid}/maps') as f:
         return [l.split() for l in f.readlines()]
 
-def filter_out_not_files(map_line):
+
+def filter_out_not_files(map_line) -> bool:
     return len(map_line) > 5 and path.isfile(map_line[5])
+
 
 def main():
     args = parse_args()
@@ -20,6 +26,7 @@ def main():
     file_maps = filter(filter_out_not_files, maps)
     for fm in file_maps:
         print(*fm)
+
 
 if __name__ == '__main__':
     main()
--- fproc.py
+++ fproc.py
@@ -1,45 +1,50 @@
 #!/bin/python3
 import argparse
-from os import path, listdir, stat
+from os import listdir, path, stat
 from pathlib import PosixPath
 from stat import S_IREAD, S_IWRITE
+from typing import List, Generator, Literal
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument('path', help='list PIDs that have opened the file at this path', type=PosixPath)
+    parser.add_argument(
+        'path', help='list PIDs that have opened the file at this path', type=PosixPath)
     return parser.parse_args()
 
-def enumerate_pids() -> int:
+
+def enumerate_pids() -> Generator[int, None, None]:
     return map(int, filter(lambda x: x.isdigit(), listdir('/proc')))
 
-def enumerate_files(pid):
+
+def enumerate_fds(pid: int) -> Generator[str, None, None]:
     return map(lambda x: f'/proc/{pid}/fd/{x}', listdir(f'/proc/{pid}/fd'))
 
-def has_file_open(pid, fpath: PosixPath): 
+
+def has_file_open(pid: int, fpath: PosixPath) -> bool:
     for f in listdir(f'/proc/{pid}/fd'):
         if path.realpath(f'/proc/{pid}/fd/{f}') == str(fpath.absolute()):
             return True
     return False
-        
 
-def get_file_attrs(pid, fpath: PosixPath): 
+
+def get_file_attrs(pid: int, fpath: PosixPath) -> Literal['--', 'r-', '-w', 'rw']:
     read, write = False, False
-    for f_path in enumerate_files(pid):
-        if path.realpath(f_path) == str(fpath.absolute()):
-            prem = stat(f_path, follow_symlinks=False).st_mode & 0o777
+    for fd in enumerate_fds(pid):
+        if path.realpath(fd) == str(fpath.absolute()):
+            prem = stat(fd, follow_symlinks=False).st_mode & 0o777
             read |= prem & S_IREAD == S_IREAD
             write |= prem & S_IWRITE == S_IWRITE
-        if read and write:
-            return 'rw'
+            if read and write:
+                return 'rw'
     return (read and 'r' or '-') + (write and 'w' or '-')
+
 
 def main():
     args = parse_args()
-    result = {f'{pid} {get_file_attrs(pid, args.path)}' 
-              for pid in enumerate_pids() 
+    result = {f'{pid} {get_file_attrs(pid, args.path)}'
+              for pid in enumerate_pids()
               if has_file_open(pid, args.path)}
     print(*result)
-
 
 
 if __name__ == '__main__':
